390586a4d52b655d7fc73a0398e0b1d4f1538f7d
SUBHANQURESHI0 <subhan.qureshi1915@gmail.com>
2024-06-28 08:26:10 +0500

    ok

---
+++ sanity/schemaTypes/blog.ts
@@ -0,0 +1,40 @@
+export default {
+    name: 'news',
+    type: 'document',
+    title: 'News',
+    fields: [
+      {
+        name: 'title',
+        type: 'string',
+        title: 'Title of news article',
+      },
+      {
+        name: 'slug',
+        type: 'slug',
+        title: 'Slug of your news article',
+        options: {
+          source: 'title',
+        },
+      },
+      {
+        name: 'titleImage',
+        type: 'image',
+        title: 'Title Image',
+      },
+      {
+        name: 'smallDescription',
+        type: 'text',
+        title: 'Small Description',
+      },
+      {
+        name: 'content',
+        type: 'array',
+        title: 'Content',
+        of: [
+          {
+            type: 'block',
+          },
+        ],
+      },
+    ],
+  }
